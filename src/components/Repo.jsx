e49405a13dd0a23cd0ade0b9f175353ee77fc3ea
Kate Matthews <katesmatthews@gmail.com>
2020-05-17 18:24:19 -0400

    Destructure props to get repo info. Display repo name, and add some styling

--- src/components/Repo.jsx
+++ src/components/Repo.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const RepoCard = styled.div``;
+const RepoCard = styled.div`
+  padding: 5px;
+  border: 1px solid grey;
+  border-radius: 3px;  
+  margin: 5px;
+  :hover {
+    border: 1px solid black;
+  }
+`;
 
-function Repo() {
+function Repo({ repoId, name, issuesURL }) {
   return (
     <RepoCard>
-      Repo here
+      {name}
     </RepoCard>
   );
 }
