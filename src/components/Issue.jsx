8f48b0afdda4487b2d56b71f2adeef06a4f20d68
Kate Matthews <katesmatthews@gmail.com>
2020-05-17 19:33:01 -0400

    Build out issue card. Present issue info, add basic styling

--- src/components/Issue.jsx
+++ src/components/Issue.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
+import noAssignee from '../img/no-assignee.png';
 
-const IssueCard = styled.div``;
+const IssueCard = styled.div`
+  border: 1px solid #6e6e6e;
+  border-radius: 3px;
+  padding: 5px;
+  display: flex-column;
+  justify-content: space-between;
+  margin: 5px;
+`;
 
-function Issue() {
+const Avatar = styled.img`
+  width: 40px;
+  margin-right: 20px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  padding: 5px;
+  // justify-content: space-between;
+  align-items: center;
+  height: 40px;
+  background-color: #e0e0e0;
+`;
+
+const Title = styled.h4`
+  text-align: center;
+`;
+
+function Issue({ issueId, avatarURL, title, created, updated }) {
   return (
     <IssueCard>
-      Issue here
+      <Header>
+        <Avatar src={avatarURL ? avatarURL : noAssignee} />
+        <Title> {title} </Title>
+      </Header>
+      <div> {`Created: ${created}`} </div>
+      <div> {`Updated: ${updated}`} </div>
     </IssueCard>
     );
 }
