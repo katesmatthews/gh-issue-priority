70d3a1f6afb09ad65ea6f665027277dd313ec964
Kate Matthews <katesmatthews@gmail.com>
2020-05-17 17:56:29 -0400

    Add repo reducer to combinereducers

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import keyReducer from './keyReducer';
+import repoReducer from './repoReducer';
 
 const rootReducer = combineReducers({
   key: keyReducer,
+  repos: repoReducer,
 });
 
 export default rootReducer;
