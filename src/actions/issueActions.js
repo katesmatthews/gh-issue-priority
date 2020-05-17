2c69a96c41a5c09fcbf0bdc1a9c415e8e6a0a513
Kate Matthews <katesmatthews@gmail.com>
2020-05-17 18:45:32 -0400

    Add inital actions and reducer for issues

---
+++ src/actions/issueActions.js
@@ -0,0 +1,21 @@
+import * as types from '../actions/actionTypes';
+
+export const getIssues = apiKey => dispatch => {
+  dispatch({ type: types.REQUEST_ISSUES_PENDING });
+  fetch('', {
+    headers: { Authorization: `token ${apiKey}`}
+  })
+    .then(res => {
+      if (res.status !== 200) {
+        console.error('Unable to retrieve issues');
+        dispatch({ type: types.REQUEST_ISSUES_FAILURE, err: res.status });
+      } else return res.json();
+    })
+    .then(issues => dispatch({ type: types.REQUEST_ISSUES_SUCCESS, payload: issues }))
+    .catch(err => dispatch({ type: types.REQUEST_ISSUES_FAILURE, err: err }));
+};
+
+export const setIssueOrder = newIssueOrder => ({
+  type: types.UPDATE_ISSUES_ORDER,
+  payload: newIssueOrder,
+});
---
+++ src/reducers/issueReducer.js
@@ -0,0 +1,21 @@
+import * as types from '../actions/actionTypes';
+
+const initialState = {
+  list: null,
+  isPending: false,
+}
+
+export const issueReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case types.REQUEST_ISSUES_PENDING:
+      return { ...state, isPending: true };
+    case types.REQUEST_ISSUES_SUCCESS:
+      return { ...state, isPending: false, list: action.payload };
+    case types.REQUEST_ISSUES_FAILURE:
+      return { ...state, isPending: false, err: action.payload };
+    case types.UPDATE_ISSUES_ORDER:
+      return { ...state, isPending: false, list: action.payload };
+    default:
+      return state;
+  }
+}
