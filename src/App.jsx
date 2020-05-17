a00678d628d033e237d53bba05004373f1405c3b
Kate Matthews <katesmatthews@gmail.com>
2020-05-17 19:50:39 -0400

    Position header / input components

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 import TokenInput from './components/TokenInput';
 import MainContainer from './containers/MainContainer';
+import './App.css';
 
 const AppContainer = styled.div``;
 
-const Header = styled.div``;
+const Header = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const Title = styled.h1``;
 
