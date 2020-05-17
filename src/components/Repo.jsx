749cc30b0910992c3a025e193dbdd76ca4359331
Kate Matthews <katesmatthews@gmail.com>
2020-05-17 18:33:40 -0400

    Add logic to repo cards to allow selecting and updating store, plus some styling

--- src/components/Repo.jsx
+++ src/components/Repo.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useSelector, useDispatch } from 'react-redux';
+import { setSelectedRepo } from '../actions/repoActions';
+
 
 const RepoCard = styled.div`
   padding: 5px;
-  border: 1px solid grey;
+  border: 1px solid #9c9c9c;
   border-radius: 3px;  
+  background-color: ${({selected}) => selected ? '#c4c4c4' : '' };
   margin: 5px;
   :hover {
     border: 1px solid black;
@@ -12,8 +16,20 @@ const RepoCard = styled.div`
 `;
 
 function Repo({ repoId, name, issuesURL }) {
+
+  const dispatch = useDispatch();
+  const selectedRepo = useSelector(state => state.repos.repoId);
+
+  function handleSelect() {
+    dispatch(setSelectedRepo(repoId));
+    // dispatch action to retrieve issues
+  }
+
   return (
-    <RepoCard>
+    <RepoCard
+      onClick={handleSelect}
+      selected={selectedRepo === repoId}
+    >
       {name}
     </RepoCard>
   );
