d4233988a9e288187f22a1389105391908e1d8b2
Kate Matthews <katesmatthews@gmail.com>
2020-05-17 18:56:55 -0400

    Add issues reducer to rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import keyReducer from './keyReducer';
 import repoReducer from './repoReducer';
+import issueReducer from './issueReducer';
+
 
 const rootReducer = combineReducers({
   key: keyReducer,
   repos: repoReducer,
+  issues: issueReducer,
 });
 
 export default rootReducer;
