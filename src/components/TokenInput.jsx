869eae1f8e6dbe3a5bc9d1383369aea363b02d0c
Kate Matthews <katesmatthews@gmail.com>
2020-05-17 17:26:48 -0400

    Set up basic controlled input and handler function, in preparation of connecting to Redux store

--- src/components/TokenInput.jsx
+++ src/components/TokenInput.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 
 const InputContainer = styled.div``;
 
+const Input = styled.input``;
+const Button = styled.button``;
+
 function TokenInput() {
+
+  const [keyInput, setKeyInput] = useState('');
+
+  const dispatch = useDispatch();
+
+  function handleSubmit() {
+    // dispatch actions to set API key in store and get repos
+    console.log(`setting API key to: ${keyInput}`)
+    setKeyInput('');  
+  }
+
   return (
     <InputContainer>
-      Input here
+      <Input
+        onChange={e => setKeyInput(e.target.value)}
+        onKeyDown={e => { if (e.key === 'Enter') handleSubmit()}}
+        placeholder="Enter GitHub API Key"
+        type="password"
+        value={keyInput}
+      />
+      <Button
+        onClick={handleSubmit}
+      >
+        Get Repos
+      </Button>
     </InputContainer>
   );
 }
