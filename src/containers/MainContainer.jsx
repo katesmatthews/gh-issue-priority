5cf87e84a5e6f4f292b30723690ff0a5753d04ad
Kate Matthews <katesmatthews@gmail.com>
2020-05-17 18:07:37 -0400

    Connect maincontainer to store, add logic to render depending on status of retrieved repos

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
 import RepoContainer from './RepoContainer';
 import IssueContainer from './IssueContainer';
 
@@ -7,7 +8,17 @@ import IssueContainer from './IssueContainer';
 const Main = styled.div``;
 
 function MainContainer() {
-  
+
+  const repoList = useSelector(state => state.repos.list);
+  const reposPending = useSelector(state => state.repos.isPending);
+  const repoError = useSelector(state => state.repos.err);
+
+  // If repos are loading / not available, render accordingly
+  if (reposPending) return <div> Loading repos... </div>;
+  if (repoError) return <div> Error loading repos! Try again. </div>;
+  if (!repoList) return null;
+  if (!repoList.length) return <div> No repos found </div>
+
   return (
     <Main>
       <RepoContainer/>
