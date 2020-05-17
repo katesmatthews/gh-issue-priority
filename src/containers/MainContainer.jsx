bcd36482efe53a1c39ae157a34d9724e977dc3ba
Kate Matthews <katesmatthews@gmail.com>
2020-05-17 17:40:19 -0400

    Add newline to end of file

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 import RepoContainer from './RepoContainer';
 import IssueContainer from './IssueContainer';
 
+
 const Main = styled.div``;
 
 function MainContainer() {
+  
   return (
     <Main>
       <RepoContainer/>
